535490f4ceb8460ef2f75afc3328715d98117165
Dennis Roberts <dennis@iplantcollaborative.org>
2011-12-31 21:31:51 -0700

    CORE-2713: begin separating category CRUD operations into a separate file

--- src/conrad/category_listings.clj
+++ src/conrad/category_listings.clj
@@ -1,35 +1,8 @@
 (ns conrad.category-listings
   (:use [conrad.database]
-        [conrad.app-listings])
+        [conrad.app-listings]
+        [conrad.category-crud])
   (:require [clojure.java.jdbc :as jdbc]))
-
-(declare load-category load-subcategories)
-
-(defn- count-templates [hid subcategories]
-  (jdbc/with-query-results rs
-    ["SELECT COUNT(*) FROM template_group_template
-      WHERE template_group_id = ?" hid]
-    (+ (:count (first rs)) (reduce + (map #(:template_count %) subcategories)))))
-
-(defn- load-category [hid & fs]
-  (jdbc/with-query-results rs
-    ["SELECT * FROM analysis_group_listing WHERE hid = ?" hid]
-    (let [f (first fs)
-          subcategories (load-subcategories hid f)
-          category (assoc (first rs)
-                     :template_count (count-templates hid subcategories)
-                     :groups subcategories)]
-      (if (nil? f) category (f category)))))
-
-(defn- load-subcategories [hid f]
-  (jdbc/with-query-results rs
-    ["SELECT * FROM template_group_group WHERE parent_group_id = ?" hid]
-    (doall (map #(load-category (:subgroup_id %) f) rs))))
-
-(defn- get-public-root-category-hids []
-  (jdbc/with-query-results rs
-    ["SELECT * FROM workspace WHERE is_public IS TRUE"]
-    (doall (map #(:root_analysis_group_id %) rs))))
 
 (defn- get-app-ids-in-category [category-hid]
   (jdbc/with-query-results rs
