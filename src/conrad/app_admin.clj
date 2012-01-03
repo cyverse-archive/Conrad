3c4ad421d104b2b03b27018e122749b71c4936f2
Dennis Roberts <dennis@iplantcollaborative.org>
2012-01-03 16:58:39 -0700

    CORE-2713: still more database access consolidation

--- src/conrad/app_admin.clj
+++ src/conrad/app_admin.clj
@@ -6,8 +6,7 @@
         [conrad.category-crud]
         [conrad.category-listings])
   (:require [clojure.java.jdbc :as jdbc]
-	    [clojure-commons.json :as cc-json])
-  (:import [java.sql Timestamp Types]))
+	    [clojure-commons.json :as cc-json]))
 
 (defn- load-transformation-activity [id]
   (let [transformation-activity (load-app-by-id id)]
@@ -15,54 +14,25 @@
       (throw (IllegalArgumentException. (str "app, " id ", not found"))))
     transformation-activity))
 
-(defn- sql-timestamp [time]
-  (if (or (nil? time) (= 0 time)) nil (Timestamp. time)))
+(defn- app-info->app-update [app-info]
+  {:name (:name app-info)
+   :description (:description app-info)
+   :integration_date (:integration_date app-info)
+   :wikiurl (:wiki_url app-info)})
 
-(defn- update-transformation-activity [app-info id]
-  (jdbc/update-values
-   :transformation_activity ["id = ?" id]
-   {:name (:name app-info)
-    :description (:description app-info)
-    :integration_date (sql-timestamp (:integration_date app-info))
-    :wikiurl (:wiki_url app-info)}))
-
-(defn- update-integration-data [id app-info]
-  (jdbc/update-values
-   :integration_data ["id = ?" id]
-   {:integrator_name (:integrator_name app-info)
-    :integrator_email (:integrator_email app-info)}))
+(defn- app-info->integration-data-update [app-info]
+  {:integrator_name (:integrator_name app-info)
+   :integrator_email (:integrator_email app-info)})
 
 (defn- update-app-info [app-info]
   (let [id (:id app-info)
         transformation-activity (load-transformation-activity id)
         hid (:hid transformation-activity)
         integration-data-id (:integration_data_id transformation-activity)]
-    (update-transformation-activity app-info id)
-    (update-integration-data integration-data-id app-info)
+    (update-transformation-activity (app-info->app-update app-info) id)
+    (update-integration-data integration-data-id
+                             (app-info->integration-data-update app-info))
     (success-response {:application (load-app-listing hid)})))
-
-(defn- mark-app-deleted [id]
-  (let [transformation-activity (load-transformation-activity id)
-        hid (:hid transformation-activity)]
-    (jdbc/update-values
-     :transformation_activity ["id = ?" id]
-     {:deleted true})
-    (success-response {:id id})))
-
-(defn- remove-public-categorizations [app-hid]
-  (jdbc/delete-rows
-   :template_group_template
-   ["template_id = ? AND template_group_id IN (
-         SELECT tg.hid FROM template_group tg
-         JOIN workspace w ON tg.workspace_id = w.id
-         WHERE w.is_public IS TRUE
-     )" app-hid]))
-
-(defn- categorize-app [app-hid category-hid]
-  (jdbc/insert-values
-   :template_group_template
-   [:template_id :template_group_id]
-   [app-hid category-hid]))
 
 (defn- move-app-to-new-category [categorization-info]
   (let [app-id (extract-required-field categorization-info :id)
@@ -71,8 +41,7 @@
         category (load-category-by-id category-id)
         app-hid (:hid transformation-activity)
         category-hid (:hid category)]
-    (remove-public-categorizations app-hid)
-    (categorize-app app-hid category-hid)
+    (move-public-app app-hid category-hid)
     (success-response {:category (list-category-with-apps category-id)})))
 
 (defn update-app [body]
@@ -81,7 +50,10 @@
 
 (defn delete-app [id]
   (jdbc/with-connection (db-connection)
-    (jdbc/transaction (mark-app-deleted id))))
+    (jdbc/transaction
+     (load-transformation-activity id)
+     (mark-app-deleted id)
+     (success-response {:id id}))))
 
 (defn move-app [body]
   (jdbc/with-connection (db-connection)
--- src/conrad/app_crud.clj
+++ src/conrad/app_crud.clj
@@ -1,6 +1,7 @@
 (ns conrad.app-crud
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log])
+  (:import [java.sql Timestamp]))
 
 (defn load-deployed-components-for-app [app-hid]
   (jdbc/with-query-results rs
@@ -18,3 +19,36 @@
   (jdbc/with-query-results rs
     ["SELECT * FROM transformation_activity WHERE id = ?" id]
     (first rs)))
+
+(defn- sql-timestamp [time]
+  (if (or (nil? time) (= 0 time)) nil (Timestamp. time)))
+
+(defn update-transformation-activity [app-update id]
+  (jdbc/update-values :transformation_activity ["id = ?" id] app-update))
+
+(defn update-integration-data [id integration-data-update]
+  (jdbc/update-values :integration_data ["id = ?" id] integration-data-update))
+
+(defn mark-app-deleted [id]
+  (jdbc/update-values
+   :transformation_activity ["id = ?" id]
+   {:deleted true}))
+
+(defn- remove-public-categorizations [app-hid]
+  (jdbc/delete-rows
+   :template_group_template
+   ["template_id = ? AND template_group_id IN (
+         SELECT tg.hid FROM template_group tg
+         JOIN workspace w ON tg.workspace_id = w.id
+         WHERE w.is_public IS TRUE
+     )" app-hid]))
+
+(defn- categorize-app [app-hid category-hid]
+  (jdbc/insert-values
+   :template_group_template
+   [:template_id :template_group_id]
+   [app-hid category-hid]))
+
+(defn move-public-app [app-hid category-hid]
+  (remove-public-categorizations app-hid)
+  (categorize-app app-hid category-hid))
