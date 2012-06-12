abbf207ae2c9a0b66482610b6bf6769b4a1e0ca0
Roey Chasman <rchasman@email.arizona.edu>
2012-06-12 12:13:13 -0700

    Really fixed the reversion and commit of an earlier genome_reference.clj.

--- src/conrad/genome_reference.clj
+++ src/conrad/genome_reference.clj
@@ -1,7 +1,7 @@
 (ns conrad.genome-reference
   (:use
     [conrad.kormadb]
-    [conrad.core]
+    [conrad.config]
     [clojure.data.json :only (json-str)]
     [korma.core]
     [korma.db]
@@ -13,6 +13,7 @@
     [java.sql Timestamp]))
 
 ;---------------------------------Helper Functions------------------------------------------
+
 (defn get-id
   "This function takes in a username and performs an sql query which finds the users id, and returns it."
   [username]
@@ -30,29 +31,21 @@
   [attrs]
   (let [username (get-user-domain attrs)]
   (let [id (get-id username)]
-    (if (nil? id)
-        (do (insert users (values {:username username}))
-            (get-id username)))id)))
-
-(defn get-name
-  "This function takes in a userID and performs an sql query getting out the username, and returning it."
-  [id]
-      (:username (first (select users
-                          (fields :username)
-                          (where {:id id})))))
+      (if (nil? id)
+          (do (insert users (values {:username username}))
+              (get-id username))) id)))
 
 (defn json-parser
-  "This function takes in a sequence and parses non-strings into strings, nil values into empty strings, and Timestamp objects into epoch seconds for JSON encoding compliance."
+  "This function takes in a sequence and parses numbers into strings, nil values into empty strings, and Timestamp objects into epoch seconds into strings. For JSON encoding compliance."
   [body]
   {:genomes (mapv #(assoc % :created_by (or (get-name (:created_by %)) "")
                             :last_modified_by (or (get-name (:last_modified_by %)) "")
                             :created_on (str (.getTime (:created_on %)))
-                            :deleted (str (:deleted %))
                             :id (str (:id %))
                             :last_modified_on (or (:last_modified_on %) "")) body)})
 
 (defn uuid-gen
-  "Auto generates a Unique Universal ID for new genome reference records."
+  "This function helps new genome reference record insertion by autogenerating a compliant Universal Unique ID."
   []
   (str (java.util.UUID/randomUUID)))
 
@@ -66,12 +59,8 @@
 (defn get-genome-references
   "This function returns a JSON representation of the map of all the genome_reference table data, skipping 'deleted' records."
   []
-  (json-str (json-parser (select genome_reference (where {:deleted false})))))
-
-(defn get-genome-reference-by-uuid
-  "This function returns a JSON representation of the genome_reference specified by the passed uuid, skipping 'deleted' records."
-  [id]
-  (json-str (json-parser (select genome_reference (where {:uuid id})))))
+  (json-str (json-parser (select genome_reference
+                            (where {:deleted false})))))
 
 (defn get-genome-references-by-username
   "This function returns a JSON representation of the map of all the genome_reference table data in the DB that was created by the passed username."
@@ -81,26 +70,29 @@
                             (join users (= :users.id :genome_reference.created_by))
                             (where {:users.username username :deleted false})))))
 
-(defn delete-genome-reference-by-UUID
-  "This function updates the deleted column of the genome_reference that matches the passed UUID's genome_reference table to true."
+(defn delete-genome-references-by-UUID
+  "This function updates the deleted column of the genome_reference that matches the passed UUID's genome_reference table to true, 'deleting' the reference."
   [uuid]
   (log/warn "UUID Passed =" uuid)
-  (update genome_reference(set-fields {:deleted true})(where {:uuid uuid})))
+  (update genome_reference
+    (set-fields {:deleted true})
+    (where {:uuid uuid})))
 
 (defn insert-genome-reference
-  "This function adds a genome-reference to the database taking a JSON object containing only the genome name and the path. TODO: created_by autogeneration should be generated sans JSON."
-  [body]
+  "This function adds a genome-reference to the database taking a JSON object containing the genome name and the path. The uuid is generated automatically, and the created_by info is pulled from the CAS info of the request map."
+  [body attrs]
   (def data (cc-json/body->json body))
   (log/warn "JSON Object Passed=" data)
-  (let [uuid (uuid-gen) name (:name data) path (:path data) cb (:created_by data)]
-    (insert genome_reference (values [{:uuid uuid :name name :path path :created_by cb}]))))
+  (let [uuid (uuid-gen) name (:name data) path (:path data) cb (get-or-create-id attrs)]
+    (insert genome_reference
+      (values [{:uuid uuid :name name :path path :created_by cb}]))))
 
-  (defn modify-genome-reference
-  "This function modifies an existing genome-reference in the database. It takes a JSON object containing the new genome name, path, and UUID."
-  [body req]
+(defn modify-genome-reference
+  "This function modifies an existing genome-reference in the database. It takes a JSON object containing the new genome name and path, and the existing UUID to identify it."
+  [body attrs]
   (def data (cc-json/body->json body))
   (log/warn "JSON Object Passed=" data)
-  (let [uuid (:uuid data) name (:name data) id (get-in req [:user-attributes :uid]) path (:path data)]
+  (let [uuid (:uuid data) name (:name data) cb (get-or-create-id attrs) path (:path data)]
     (update genome_reference
-      (where {:uuid uuid :id id}) ;need to get a way to get the uid from the passed from req username.
+      (where {:created_by cb :uuid uuid})
       (set-fields {:name name :path path}))))
