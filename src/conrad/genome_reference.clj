5e682a6179106b9f12b8bfd968db4cb32bcbe1da
Roey Chasman <rchasman@email.arizona.edu>
2012-06-11 17:00:11 -0700

    Clarified some of the documentation, prettied up some formatting, and encapsulated some repeated code.

--- src/conrad/genome_reference.clj
+++ src/conrad/genome_reference.clj
@@ -14,13 +14,6 @@
 
 ;---------------------------------Helper Functions------------------------------------------
 
-(defn get-name
-  "This function takes in a user id and performs an sql query which finds the username, and returns it."
-  [id]
-  (:username (first (select users
-                      (fields :username)
-                      (where {:id id})))))
-
 (defn get-id
   "This function takes in a username and performs an sql query which finds the users id, and returns it."
   [username]
@@ -28,16 +21,22 @@
                 (fields :id)
                 (where {:username username})))))
 
+(defn get-user-domain
+  "this function appends the domain set in the config.clj to the request mapped username."
+  [attrs]
+  (str (attrs "uid") "@" (uid-domain)))
+
 (defn get-or-create-id
-  "This function will call get-id and if it returns nil it will create a new user and return that id."
-  [username]
+  "This function will call get-id and if it returns nil it will create a new user and return that id, otherwise it will just return the id."
+  [attrs]
+  (let [username (get-user-domain attrs)]
   (let [id (get-id username)]
     (if (nil? id)
-        (do (insert users (values {:username username}))
-            (get-id username)) id)))
+      (do (insert users (values {:username username}))
+          (get-id username))) id)))
 
 (defn json-parser
-  "This function takes in a sequence and parses non-strings into strings, nil values into empty strings, and Timestamp objects into epoch seconds for JSON encoding compliance."
+  "This function takes in a sequence and parses numbers into strings, nil values into empty strings, and Timestamp objects into epoch seconds into strings. For JSON encoding compliance."
   [body]
   {:genomes (mapv #(assoc % :created_by (or (get-name (:created_by %)) "")
                             :last_modified_by (or (get-name (:last_modified_by %)) "")
@@ -46,7 +45,7 @@
                             :last_modified_on (or (:last_modified_on %) "")) body)})
 
 (defn uuid-gen
-  "Auto generates a Unique Universal ID for new genome reference records."
+  "This function helps new genome reference record insertion by autogenerating a compliant Universal Unique ID."
   []
   (str (java.util.UUID/randomUUID)))
 
@@ -72,7 +71,7 @@
                             (where {:users.username username :deleted false})))))
 
 (defn delete-genome-references-by-UUID
-  "This function updates the deleted column of the genome_reference that matches the passed UUID's genome_reference table to true."
+  "This function updates the deleted column of the genome_reference that matches the passed UUID's genome_reference table to true, 'deleting' the reference."
   [uuid]
   (log/warn "UUID Passed =" uuid)
   (update genome_reference
@@ -80,20 +79,20 @@
     (where {:uuid uuid})))
 
 (defn insert-genome-reference
-  "This function adds a genome-reference to the database taking a JSON object containing the genome name and the path. The uuid is generated automatically, and the created_by info is pulled from the CAS request map."
+  "This function adds a genome-reference to the database taking a JSON object containing the genome name and the path. The uuid is generated automatically, and the created_by info is pulled from the CAS info of the request map."
   [body attrs]
   (def data (cc-json/body->json body))
   (log/warn "JSON Object Passed=" data)
-  (let [uuid (uuid-gen) name (:name data) path (:path data) cb (get-or-create-id (str (attrs "uid") "@" (uid-domain)))]
+  (let [uuid (uuid-gen) name (:name data) path (:path data) cb (get-or-create-id attrs)]
     (insert genome_reference
       (values [{:uuid uuid :name name :path path :created_by cb}]))))
 
 (defn modify-genome-reference
-  "This function modifies an existing genome-reference in the database. It takes a JSON object containing the new genome name and path, and the existing UUID as a reference."
+  "This function modifies an existing genome-reference in the database. It takes a JSON object containing the new genome name and path, and the existing UUID to identify it."
   [body attrs]
   (def data (cc-json/body->json body))
   (log/warn "JSON Object Passed=" data)
-  (let [uuid (:uuid data) name (:name data) cb (get-or-create-id (str (attrs "uid") "@" (uid-domain))) path (:path data)]
+  (let [uuid (:uuid data) name (:name data) cb (get-or-create-id attrs) path (:path data)]
     (update genome_reference
       (where {:created_by cb :uuid uuid})
       (set-fields {:name name :path path}))))
