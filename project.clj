fcbebd08889426fa762ec31be7335f007783815b
Dennis Roberts <dennis@iplantcollaborative.org>
2012-07-06 17:43:11 -0700

    CORE-3342: upgrade to the latest version of lein-iplant-rpm

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conrad "1.1.0-SNAPSHOT"
+(defproject conrad "1.2.0-SNAPSHOT"
   :description "Back-End Services for the iPlant Administrative Console"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
@@ -14,7 +14,7 @@
                  [korma/korma "0.3.0-beta10"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [postgresql/postgresql "9.0-801.jdbc4"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.2.1-SNAPSHOT"]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]
             [lein-ring "0.7.1"]
             [swank-clojure "1.4.2"]]
   :profiles {:dev {:resource-paths ["conf/test"]}}
@@ -22,7 +22,6 @@
   :main conrad.core
   :ring {:handler conrad.core/app :init conrad.core/load-configuration}
   :iplant-rpm {:summary "iPlant Conrad"
-               :release 3
                :provides "conrad"
                :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
