01e33a9c1f0be7e601a69e44faa34078c0df1541
Dennis Roberts <dennis@iplantcollaborative.org>
2012-12-10 13:04:47 -0700

    CORE-3645: change the port number that is used when running the service using lein ring server

--- project.clj
+++ project.clj
@@ -20,7 +20,9 @@
   :profiles {:dev {:resource-paths ["conf/test"]}}
   :aot [conrad.core]
   :main conrad.core
-  :ring {:handler conrad.core/app :init conrad.core/load-configuration-from-file}
+  :ring {:handler conrad.core/app
+         :init conrad.core/load-configuration-from-file
+         :port 31334}
   :iplant-rpm {:summary "iPlant Conrad"
                :provides "conrad"
                :dependencies ["iplant-service-config >= 0.1.0-5"]
