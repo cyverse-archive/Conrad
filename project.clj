14f264463641d9a45003c49ad6186c8ac3adf193
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-29 14:11:06 -0700

    CORE-3924: migrated to Korma 0.3.0-RC2

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject conrad "1.2.0-SNAPSHOT"
+(defproject conrad "1.3.0-SNAPSHOT"
   :description "Back-End Services for the iPlant Administrative Console"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.clojure/java.jdbc "0.1.3"]
+                 [org.clojure/java.jdbc "0.2.3"]
                  [org.iplantc/clj-cas "1.0.0-SNAPSHOT"]
-                 [org.iplantc/kameleon "0.0.1-SNAPSHOT"]
+                 [org.iplantc/kameleon "0.1.0-SNAPSHOT"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [compojure "1.0.1"]
                  [swank-clojure "1.4.0-SNAPSHOT"]
                  [log4j/log4j "1.2.16"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [korma/korma "0.3.0-beta10"]
+                 [korma/korma "0.3.0-RC2"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [postgresql/postgresql "9.0-801.jdbc4"]]
   :plugins [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]
